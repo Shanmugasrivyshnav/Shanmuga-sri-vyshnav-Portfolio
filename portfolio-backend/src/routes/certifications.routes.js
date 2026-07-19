0ee9acb28de79e731f75aaf73ca8dc3bd94c52c6
shanmuga <shanmukhasri00@gmail.com>
2026-07-19 10:30:12 +0530

    remove backend file then adjust app logo

--- portfolio-backend/src/routes/certifications.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as ctrl from "../controllers/certifications.controller.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-
-const router = Router();
-
-router.get("/", asyncHandler(ctrl.listCertifications));
-
-export default router;
--- portfolio-backend/src/routes/skills.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as ctrl from "../controllers/skills.controller.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-
-const router = Router();
-
-router.get("/", asyncHandler(ctrl.listSkills));
-
-export default router;
